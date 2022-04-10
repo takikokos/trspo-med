b48e3a61e3983e34152af299c3c488e985fd88f2
Nikita Lebiodkin <lebiodkin.nick@yandex.ru>
2022-04-10 21:04:06 +0300

    interface and authorization

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+
+#include "vaccinetable.h"
+#include "authdlg.h"
+
 #include <QFileDialog>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,17 +13,51 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     vaccines = new VaccineTable(this);
-    ui->tableView->setModel(vaccines);
-//    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+//    ui->tableView->setModel(vaccines);
+//    ui->tableView->resizeColumnsToContents();
 
+    ui->openFileButton->setEnabled(false);
+    ui->exitButton->setEnabled(false);
+//    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_authButton_clicked()
+{
+    AuthDlg authDlg(this);
+
+    if (authDlg.exec() != QDialog::Accepted)
+        return;
+
+    ui->authButton->setEnabled(false);
+    ui->exitButton->setEnabled(true);
+
+    QString user = authDlg.getUser();
+    if (user == "admin")
+    {
+        ui->openFileButton->setEnabled(true);
+    }
+}
+
+void MainWindow::on_openFileButton_clicked()
+{
     QString fileName = QFileDialog::getOpenFileName(this,
-        tr("Open table"), QCoreApplication::applicationDirPath(), tr("Table Data (*.csv)")
+        tr("Открыть таблицу"), QCoreApplication::applicationDirPath(), tr("Table Data (*.csv)")
     );
     QFile file(fileName);
 
     if ( !file.open(QIODevice::ReadOnly | QIODevice::Text) ) {
         qDebug() << "File not exists";
     } else {
+        vaccines->clear();
+
+        ui->tableView->setModel(vaccines);
+        ui->tableView->resizeColumnsToContents();
+
         QTextStream in(&file);
         in.readLine(); // skip header
         vaccines->load_from_text(in);
@@ -27,9 +66,12 @@ MainWindow::MainWindow(QWidget *parent)
     }
 }
 
-MainWindow::~MainWindow()
+void MainWindow::on_exitButton_clicked()
 {
-    delete ui;
-    delete vaccines;
+    ui->authButton->setEnabled(true);
+    ui->exitButton->setEnabled(false);
+    ui->openFileButton->setEnabled(false);
+
+    vaccines->clear();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,12 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "vaccinetable.h"
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+class VaccineTable;
 
 class MainWindow : public QMainWindow
 {
@@ -16,8 +17,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_authButton_clicked();
+
+    void on_openFileButton_clicked();
+
+    void on_exitButton_clicked();
+
 private:
     Ui::MainWindow *ui;
-    VaccineTable *vaccines;
+    VaccineTable *vaccines = nullptr;
 };
 #endif // MAINWINDOW_H
